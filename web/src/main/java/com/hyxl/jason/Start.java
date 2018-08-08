07ce2945e6aaa647885d907d8bfc7115abdca43d
HX0010080 <447312556@qq.com>
2018-08-08 14:22:03 +0800

    修复XXbug

---
+++ web/src/main/java/com/hyxl/jason/Start.java
@@ -0,0 +1,11 @@
+package com.hyxl.jason;
+
+/**
+ * Created by HX0010080 on 2018/8/8 0008.
+ */
+public class Start {
+    public static void main(String[] args){
+
+        System.out.println("haha");
+    }
+}
